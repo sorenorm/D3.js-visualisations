126f3838b96ff49bb1dc205642cd34f99a98a4b9
Søren Orm <54844057+sorenorm@users.noreply.github.com>
2024-09-30 11:10:21 +0100

    added slider, d3-tip, continent selector, play/pause, and reset

--- mindTheGap_dynamic/js/main.js
+++ mindTheGap_dynamic/js/main.js
@@ -10,12 +10,28 @@ const HEIGHT = 500 - MARGIN.TOP - MARGIN.BOTTOM
 
 const svg = d3.select("#chart-area").append("svg")
   .attr("width", WIDTH + MARGIN.LEFT + MARGIN.RIGHT)
-  .attr("height", 2 * HEIGHT + MARGIN.TOP + 2 * MARGIN.BOTTOM)
+  .attr("height", HEIGHT + MARGIN.TOP + MARGIN.BOTTOM)
 
 const g = svg.append("g")
   .attr("transform", `translate(${MARGIN.LEFT}, ${MARGIN.TOP})`)
 
+
 let time = 0
+let interval
+let formattedData
+
+// Tooltip
+const tip = d3.tip()
+  .attr('class', 'd3-tip')
+	.html(d => {
+		let text = `<strong>Country:</strong> <span style='color:red;text-transform:capitalize'>${d.country}</span><br>`
+		text += `<strong>Continent:</strong> <span style='color:red;text-transform:capitalize'>${d.continent}</span><br>`
+		text += `<strong>Life Expectancy:</strong> <span style='color:red'>${d3.format(".2f")(d.life_exp)}</span><br>`
+		text += `<strong>GDP Per Capita:</strong> <span style='color:red'>${d3.format("$,.0f")(d.income)}</span><br>`
+		text += `<strong>Population:</strong> <span style='color:red'>${d3.format(",.0f")(d.population)}</span><br>`
+		return text
+	})
+g.call(tip)
 
 g.append("text")
 		.attr("class", "x axis-label")
@@ -107,7 +123,7 @@ continents.forEach((continent, i) => {
 
 d3.json("data/data.json").then(function(data){
 	// clean data
-	const formattedData = data.map(year => {
+	formattedData = data.map(year => {
 		return year["countries"].filter(country => {
 			const dataExists = (country.income && country.life_exp)
 			return dataExists
@@ -117,27 +133,75 @@ d3.json("data/data.json").then(function(data){
 			return country
 		})
 	})
-	console.log(data);
-
+	/*
 	d3.interval(function(){
 		// at the end of our data, loop back
 		time = (time < 214) ? time + 1 : 0
 		update(formattedData[time])
 	}, 100)
-
+	*/
 	// first run of the visualization
 	update(formattedData[0])
 })
 
+function step() {
+	// at the end of our data, loop back
+	time = (time < 214) ? time + 1 : 0
+	update(formattedData[time])
+}
+
+$("#play-button")
+	.on("click", function() {
+		const button = $(this)
+		if (button.text() === "Play") {
+			button.text("Pause")
+			interval = setInterval(step, 100)
+		}
+		else {
+			button.text("Play")
+			clearInterval(interval)
+		}
+	})
+
+$("#reset-button")
+	.on("click", () => {
+		time = 0
+		update(formattedData[0])
+	})
+
+$("#continent-select")
+	.on("change", () => {
+		update(formattedData[time])
+	})
+
+$("#date-slider").slider({
+		min: 1800,
+		max: 2014,
+		step: 1,
+		slide: (event, ui) => {
+			time = ui.value - 1800
+			update(formattedData[time])
+		}
+	})
 
 function update(data) {
 	// Define transition
 	const t = d3.transition()
 		.duration(100);
 
+	const continent = $("#continent-select").val()
+
+	const filteredData = data.filter(d => {
+		if (continent === "all") return true
+		else {
+			return d.continent == continent
+		}
+	})
+
+
 	// JOIN new data with old elements
 	const circles = g.selectAll("circle")
-		.data(data, d => d.country);  // Use country name as the key for data binding
+		.data(filteredData, d => d.country);  // Use country name as the key for data binding
 
 	// EXIT old elements not present in new data
 	circles.exit().remove()
@@ -148,6 +212,8 @@ function update(data) {
 		.attr('stroke', 'black')
 		.attr("stroke-width", 1)  // Set stroke width
     	.attr("fill-opacity", 0.7)  // Set fill transparency (alpha)
+		.on("mouseover", tip.show)
+		.on("mouseout", tip.hide)
 		.merge(circles)  // MERGE enter and update selections
 		.transition(t)  // Apply transition to both new and updating elements
 			.attr("cx", d => x(d.income))  // Update x position
@@ -155,4 +221,7 @@ function update(data) {
 			.attr("r", d => radius(d.population));
 
 	timeLabel.text(String(time + 1800))
+
+	$("#year")[0].innerHTML = String(time + 1800)
+	$("#date-slider").slider("value", Number(time + 1800))
 }
